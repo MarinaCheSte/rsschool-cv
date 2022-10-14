0774f05a4a46386fa1d3725015b0e8ef7a760f9f
MarinaCheSte <raggatip@list.ru>
2022-10-14 16:23:39 +0300

    feat:skill page added

---
+++ index.js
@@ -0,0 +1,9 @@
+
+/*function visibleBlock() {
+
+classList.add('info-section__avatar',"info-section__greeting-text","button_primary","button_secondary", "button_third", "button");
+
+}
+
+document.addEventListener('DOMContentLoaded', visibleBlock);*/
+
